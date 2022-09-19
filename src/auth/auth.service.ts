e383c72b4ee761a86568aee4713f8b6907e7f041
luana.macedo <luana.macedo@ilia.digital>
2022-09-19 17:52:10 -0300

    feat: implement authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { Permission } from 'src/users/entities/enums/permission.enum';
 
 @Injectable()
 export class AuthService {
@@ -25,7 +26,7 @@ export class AuthService {
 
   async login(user: any) {
     const param = user._doc
-    const payload = { id: param._id, email: param.email };
+    const payload = { id: param._id, email: param.email, permissions: param.permissions[0] };
     return {
       access_token: this.jwtService.sign(payload),
     };
--- src/auth/roles/roles.guard.ts
+++ src/auth/roles/roles.guard.ts
@@ -6,16 +6,12 @@ import { UsersService } from '../../users/users.service';
  
 const RoleGuard = (role: Permission): Type<CanActivate> => {
   class RoleGuardMixin implements CanActivate {
-    constructor(
-        private readonly usersService: UsersService) 
-        {}
+    
    async canActivate(context: ExecutionContext) {
       const request = context.switchToHttp().getRequest<RequestWithUser>();
-      const user = request.user.email;
+      const user = request.user;
       console.log(user)
-      const userlog = await this.usersService.findEmail(user)
-      console.log(userlog)
-      return userlog?.permissions.includes(role);
+      return user?.permissions ? user?.permissions.includes(role) : false;
     }
   }
  
